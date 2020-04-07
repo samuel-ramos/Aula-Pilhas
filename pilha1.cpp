c6b53396b189c8ddeca530f511dfedad74149455
samuel-ramos <61999940+samuel-ramos@users.noreply.github.com>
2020-04-06 23:11:23 -0300

    Exercicios da Aula
    Exercicios da Aula

---
+++ pilha1.cpp
@@ -0,0 +1,16 @@
+#include<stack>
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+	stack<int> pilha;
+	
+	pilha.push(10);
+	pilha.push(9);
+	pilha.push(8);
+	pilha.push(7);
+}
+system('pause');
+return 0;
---
+++ pilha3.cpp
@@ -0,0 +1,31 @@
+#include <stack>
+#include<iostream>
+
+using namespace std;
+
+void imprime_pilha(stack <int> pilha) {
+	printf("\nPilha:\n");
+	while (!pilha.empty()){
+		printf("%d \n", pilha.top());
+		//printf (pilha.top());
+		pilha.pop();
+	}
+	printf("\n");
+}
+
+int main()
+{
+	stack<int> pilha;
+	
+	pilha.push(10);
+	pilha.push(9);
+	pilha.push(8);
+	pilha.push(7);
+	imprime_pilha(pilha);
+	pilha.pop();
+	imprime_pilha(pilha);
+	pilha.pop();
+	imprime_pilha(pilha);
+	
+}
+
